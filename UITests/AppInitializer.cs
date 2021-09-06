f76f2c3529e067b4909f83eab207f5f13b0371da
Natasha <admin@natashaosborne.com>
2021-09-06 10:18:20 -0600

    WIP - environment variables - can i set them...

---
+++ UITests/AppInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            // environment variables are not working...
+            //string installedApp = Environment.GetEnvironmentVariable("ANDROID_APP");
+            //string installedApp = ConfigurationManager.AppSettings["ANDROID_APP"];
+
+            //Console.WriteLine("GetEnvironmentVariables: SETTING: ", installedApp);
+            //Console.WriteLine("GetEnvironmentVariables: androidApp: ", installedApp);
+
+            //if (platform == Platform.Android)
+            //{
+            return ConfigureApp
+                    .Android
+                    // TODO: Update this path to point to your Android app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+                    .InstalledApp("com.natashaosborne.xamarintesting.xamarintesting")
+                    .StartApp();
+            //}
+
+            //return ConfigureApp
+            //    .iOS
+            //    // TODO: Update this path to point to your iOS app and uncomment the
+            //    // code if the app is not included in the solution.
+            //    //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/XamarinForms.iOS.app")
+            //    .InstalledApp(installedApp)
+            //    .StartApp();
+        }
+    }
+}
